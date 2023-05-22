5ce90ccc3862eab088c3635429cd6acea31d4bf8
Eunnylog <lllala0710@gmail.com>
2023-05-23 00:35:26 +0900

    :memo: product 뷰, 시리얼라이저 작성

--- articles/models.py
+++ articles/models.py
@@ -3,12 +3,12 @@ from django.core.exceptions import ValidationError
 
 
 class Product(models.Model):
-    user = models.IntegerField # 임시유저
+    user = models.IntegerField() # 임시유저
     product = models.CharField("상품명", max_length=30)
     content = models.TextField("내용")
-    price = models.PositiveIntegerField("가격")
-    image = models.ImageField("사진", blank=True)
-    total_quantity = models.PositiveIntegerField("총수량")
+    price = models.IntegerField("가격")
+    image = models.ImageField("사진", blank=True, null=True)
+    total_quantity = models.IntegerField("총수량")
     # inbound_quantity = models.PositiveIntegerField("입고량") # 필드에 넣을지 말지 고민중입니다.
     # order_quantity = models.PositiveIntegerField("주문량") # 필드에 넣을지 말지 고민중입니다.
     inventory_status = models.BooleanField("재고유무", default=True) # 품절이면 False
@@ -19,8 +19,13 @@ class Product(models.Model):
         return str(self.product)
     
     def save(self, **kwargs):
+        if self.total_quantity is None:
+            self.total_quantity = 0  # 기본값으로 0 설정 또는 다른 유효한 값으로 대체
+        
         if self.total_quantity < 0:
             raise ValidationError("총수량이 0보다 작을 수 없습니다.")
+        
         if self.total_quantity <= 0:
             self.inventory_status = False
+        
         super().save(**kwargs)
--- articles/urls.py
+++ articles/urls.py
@@ -4,5 +4,5 @@ from articles import views
 
 urlpatterns = [
     path('', views.ProductView.as_view(), name='product_view'),
-    # path('<int:id_product>/', views.ProductDetailView.as_view(), name='product_view'),
+    path('<int:id_product>/', views.ProductDetailView.as_view(), name='product_view'),
 ]
