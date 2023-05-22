36855f0c84a2b7c0ab53327e7227cd7bc43953a4
Eunnylog <lllala0710@gmail.com>
2023-05-22 20:51:36 +0900

    :memo: product 모델 작성

--- b5_ah_molrang/urls.py
+++ b5_ah_molrang/urls.py
@@ -22,8 +22,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path("users/", include("users.urls")),
-    # path("articles/", include("articles.urls")),
+    path("products/", include("articles.urls")),
 ]
 
-# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
