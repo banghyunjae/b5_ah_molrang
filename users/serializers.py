c4ae87a48eb13f847aa9c4a660953f623a51017c
choiyeji <yj970311@naver.com>
2023-05-31 09:16:22 +0900

    :bug: 회원 수정 안되는 버그 수정,마이페이지에서 프로덕트 이름이 안보이는 버그 수정

--- users/serializers.py
+++ users/serializers.py
@@ -17,10 +17,11 @@ class UserSerializer(serializers.ModelSerializer):
         user.save()
         return user
 
-    def update(self, validated_data):
-        user = super().create(validated_data)
+    def update(self, instance, validated_data):
+        # patch를 사용 하기 위에 값이 들어 오지 않으면 원래 상태, 아니면 수정된 데이터 저장
+        user = super().update(instance, validated_data)
         password = user.password
-        user.set_password(password)
+        user.set_password(password)  # 비밀번호 암호화
         user.save()
         return user
 
@@ -30,6 +31,7 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     def get_token(cls, user):
         token = super().get_token(user)
         token['username'] = user.username
+        token['email'] = user.email
         token['is_admin'] = user.is_admin
 
         return token
