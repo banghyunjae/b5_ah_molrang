ef033dbfd21b6ecd1e849e718aabeddb36087d5f
banghyunjae <banghyunjae@naver.com>
2023-06-01 17:40:41 +0900

    :bug:주석 재수정

--- carts/permissions.py
+++ carts/permissions.py
@@ -3,21 +3,19 @@ from rest_framework import permissions
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
     """
-    해당 객체의 소유자만 수정할 수 있는 권한 클래스
+    소유자만 수정할 수 있는 권한 클래스
     """
 
     def has_object_permission(self, request, view, obj):
-        # 읽기 권한은 모든 요청에 허용합니다.
         if request.method in permissions.SAFE_METHODS:
             return True
 
-        # 쓰기 권한은 카트 또는 카트 아이템의 소유자에게만 허용합니다.
         return obj.user == request.user
 
 
 class IsCartOwner(permissions.BasePermission):
     """
-    해당 카트의 소유자만 접근할 수 있는 권한 클래스
+    카트의 소유자만 접근할 수 있는 권한 클래스
     """
 
     def has_object_permission(self, request, view, obj):
@@ -26,13 +24,11 @@ class IsCartOwner(permissions.BasePermission):
 
 class IsOwnerOrAdmin(permissions.BasePermission):
     """
-    해당 카트 또는 카트 아이템의 소유자 또는 관리자만 접근하고 수정할 수 있는 권한 클래스
+    카트 또는 카트 아이템의 소유자 또는 관리자만 접근하고 수정할 수 있는 권한 클래스
     """
 
     def has_object_permission(self, request, view, obj):
-        # 관리자 사용자는 모든 접근 권한을 가집니다.
         if request.user.is_staff or request.user.is_superuser:
             return True
 
-        # 카트 또는 카트 아이템의 소유자만 접근하고 수정할 수 있습니다.
         return obj.user == request.user
--- carts/views.py
+++ carts/views.py
@@ -15,29 +15,22 @@ class CartListCreateView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        # 현재 사용자의 장바구니 아이템을 필터링합니다.
         cart_items = CartItem.objects.filter(
             cart__user=request.user).distinct()
-        # CartItemSerializer를 사용하여 장바구니 아이템을 직렬화합니다.
         serializer = CartItemSerializer(cart_items, many=True)
         return Response(serializer.data)
 
     def post(self, request):
-        # 전송된 데이터를 기반으로 CartItemListSerializer를 생성합니다.
         serializer = CartItemListSerializer(data=request.data)
         if serializer.is_valid():
-            # 현재 사용자의 카트를 가져오거나 생성합니다.
             cart, _ = Cart.objects.get_or_create(user=request.user)
             product = serializer.validated_data['product']
             quantity = serializer.validated_data['quantity']
-            # 카트에 이미 해당 상품이 있는지 확인합니다.
             cart_item = cart.cart_items.filter(product=product).first()
             if cart_item:
-                # 이미 상품이 있는 경우 수량을 업데이트합니다.
                 cart_item.quantity = quantity
                 cart_item.save()
             else:
-                # 상품이 없는 경우 새로운 CartItem을 생성합니다.
                 CartItem.objects.create(
                     cart=cart, product=product, quantity=quantity)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -46,7 +39,7 @@ class CartListCreateView(APIView):
 
 class CartRetrieveUpdateDestroyView(APIView):
     """
-    특정 장바구니 조회, 수정, 삭제를 담당합니다.
+    장바구니 조회, 수정, 삭제를 담당합니다.
     """
     permission_classes = [IsAuthenticated, IsOwnerOrAdmin]
 
@@ -57,16 +50,12 @@ class CartRetrieveUpdateDestroyView(APIView):
             raise Http404
 
     def get(self, request, pk):
-        # 주어진 pk에 해당하는 카트를 가져옵니다.
         cart = self.get_object(pk)
-        # CartSerializer를 사용하여 카트를 직렬화합니다.
         serializer = CartSerializer(cart)
         return Response(serializer.data)
 
     def put(self, request, pk):
-        # 주어진 pk에 해당하는 카트를 가져옵니다.
         cart = self.get_object(pk)
-        # 전송된 데이터를 기반으로 CartSerializer를 생성합니다.
         serializer = CartSerializer(cart, data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -74,18 +63,15 @@ class CartRetrieveUpdateDestroyView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
-        # 주어진 pk에 해당하는 카트를 가져옵니다.
         cart = self.get_object(pk)
-        # 카트에 속한 모든 아이템을 삭제합니다.
         cart.cart_items.all().delete()
-        # 카트 자체를 삭제합니다.
         cart.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class CartItemRetrieveUpdateDestroyView(APIView):
     """
-    특정 장바구니 아이템 조회, 수정, 삭제를 담당합니다.
+    장바구니 아이템 조회, 수정, 삭제를 담당합니다.
     """
     permission_classes = [IsAuthenticated, IsOwnerOrAdmin]
 
@@ -96,16 +82,12 @@ class CartItemRetrieveUpdateDestroyView(APIView):
             raise Http404
 
     def get(self, request, pk):
-        # 주어진 pk에 해당하는 카트 아이템을 가져옵니다.
         cart_item = self.get_object(pk)
-        # CartItemSerializer를 사용하여 카트 아이템을 직렬화합니다.
         serializer = CartItemSerializer(cart_item)
         return Response(serializer.data)
 
     def put(self, request, pk):
-        # 주어진 pk에 해당하는 카트 아이템을 가져옵니다.
         cart_item = self.get_object(pk)
-        # 전송된 데이터를 기반으로 CartItemSerializer를 생성합니다.
         serializer = CartItemSerializer(cart_item, data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -113,9 +95,7 @@ class CartItemRetrieveUpdateDestroyView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
-        # 주어진 pk에 해당하는 카트 아이템을 가져옵니다.
         cart_item = self.get_object(pk)
-        # 카트 아이템을 삭제합니다.
         cart_item.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
@@ -127,10 +107,8 @@ class CartTotalPriceView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        # 현재 사용자의 장바구니 아이템을 필터링합니다.
         cart_items = CartItem.objects.filter(
             cart__user=request.user).distinct()
-        # 장바구니 아이템의 가격을 모두 합산합니다.
         total_price = sum(item.product.price *
                           item.quantity for item in cart_items)
         return Response({"total_price": total_price})
@@ -138,22 +116,18 @@ class CartTotalPriceView(APIView):
 
 class CartItemSelectionView(APIView):
     """
-    특정 장바구니 아이템의 선택 여부를 설정하거나 해제합니다.
+    장바구니 아이템의 선택 여부를 설정하거나 해제합니다.
     """
     permission_classes = [IsAuthenticated]
 
     def post(self, request, pk):
-        # 주어진 pk에 해당하는 카트 아이템을 가져옵니다.
         cart_item = CartItem.objects.get(pk=pk)
-        # 카트 아이템의 선택 여부를 True로 설정합니다.
         cart_item.is_selected = True
         cart_item.save()
         return Response(status=status.HTTP_200_OK)
 
     def delete(self, request, pk):
-        # 주어진 pk에 해당하는 카트 아이템을 가져옵니다.
         cart_item = CartItem.objects.get(pk=pk)
-        # 카트 아이템의 선택 여부를 False로 설정합니다.
         cart_item.is_selected = False
         cart_item.save()
         return Response(status=status.HTTP_204_NO_CONTENT)
